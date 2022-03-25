25d5c7475ca82ccda218dbe5a40e842f03dd2639
ashleyvasquez <c.vasquezashley@gmail.com>
2022-03-25 01:29:47 -0600

    parser actualizado

--- COMP/Syntax.py
+++ COMP/Syntax.py
@@ -1,8 +1,5 @@
-import sys
 from COMP.ply import yacc
-import os
-from Lexer import token
-from sys import stdin
+from COMP.Lexer import tokens
 
 precedence = (
     ('right', 'COMMA', 'EQUALS'),
@@ -11,128 +8,96 @@ precedence = (
     ('left', 'TIMES', 'DIV', 'FULLDIV'),
     ('left', 'MODULE'),
     ('left', 'POWER'),
-    ('left', 'LPAR', 'RPAR'),
+    ('left', 'LPAR', 'RPAR', 'LBRACK', 'PBRACK'),
 
 )
 
 
-#terminales: tokens
 def p_program(p):
-    """program = block"""
-    p[0] = p.program(p[1], "program")
-    print ("program")
+    '''program : block'''
+    print("program")
 
 
-def p_statement4(p):
-    """statement: IF condition THEN statement"""
-    print ("statement 4")
+def p_statement_set(p):
+    '''statement : SET VAR COMMA NUMBER SEMICOLOMN'''
+    print("statement set")
+
+
+def p_statement_for(p):
+    '''statement : FOR VAR TO NUMBER STEP NUMBER'''
+    print("statement for")
+
+
+def p_statement_if(p):
+    """statement : IF LBRACK sentence RBRACK
+               |IF LBRACK sentence RBRACK ELSE LBRACK sentence RBRACK
+               """
+    print("statement if")
+
+
+def p_statementEnCaso(p):
+    """statement : EnCaso Cuando ID relation term EnTons LBRACK sentence RBRACK SiNo LBRACK sentence RBRACK
+    Fin-EnCaso SEMICOLOMN  | EnCaso ID Cuando relation term EnTons LBRACK sentence RBRACK SiNo LBRACK sentence RBRACK Fin-EnCaso SEMICOLOMN"""
+    print("statement 5")
 
 
 def p_statementEmpty(p):
-    """statement: empty"""
-    print ("nulo")
+    '''statement : empty'''
+    print("nulo")
 
 
-def p_statementList1(p):
-    """statement: statement"""
-    print ("statementList 1")
+def p_relation(p):
+    """relation : factor GREATER factor
+                | factor SMALLER factor
+                | factor GREATERQ factor
+                | factor SMALLERQ factor
+                """
+    print("Comparacion")
 
 
-def p_statementList2(p):
-    """statement: statementList ; statement"""
-    print ("statementList 2")
+def p_expression(p):
+    '''expression : term relation term'''
+    print("Expression")
 
 
-def p_condition2(p):
-    """condition : expression relation expression"""
-    print ("condition 2")
+def p_arithmetic(p):
+    """arithmetic : factor PLUS factor
+                | factor MINUS factor
+                | factor TIMES factor
+                | factor DIV factor
+                | factor FULLDIV factor
+                | factor MODULE factor
+                | factor POWER factor
+                """
+    print("Operación Aritmética")
 
 
-def p_relation1(p):
-    """relation : COMMA"""
-    print ("relation 1")
+def p_factor(p):
+    """factor : ID
+            | NUMBER
+            | STRING
+            | BOOL
+            | empty"""
+    print("Factor")
 
 
-def p_relation2(p):
-    """relation : GREATER"""
-    print ("relation 2")
-
-
-def p_relation3(p):
-    """relation : SMALLER"""
-    print ("relation 3")
-
-
-def p_relation4(p):
-    """relation : SMALLEQ"""
-    print ("relation 4")
-
-def p_relation5(p):
-    """relation : GREATEQ"""
-    print ("relation 5")
-
-
-
-def p_expression1(p):
-    """expression : term"""
-    print ("expression 1")
-
-
-def p_expression2(p):
-    """expression : addingOperatior term"""
-    print ("expression 2")
-
-
-def p_expression3(p):
-    """expression : expression addingOperator term"""
-    print ("expression 3")
-
-
-def p_term1(p):
-    """term : factor"""
-    print ("term 1")
-
-
-def p_term2(p):
-    """term : term multiplyingOperator factor"""
-    print ("term 2")
-
-
-def p_multiplyingOperator1(p):
-    """multiplyingOperator : TIMES"""
-    print ("multiplyingOperator 1")
-
-
-def p_multiplyingOperator2(p):
-    """multiplyingOperator : DIVIDE"""
-    print ("multiplyingOperator 2")
-
-
-def p_factor1(p):
-    """factor : ID"""
-    print ("factor 1")
-
-
-def p_factor2(p):
-    """factor : NUMBER"""
-    print ("factor 2")
-
-
-def p_factor3(p):
-    """factor : LPARENT expression RPARENT"""
-    print ("factor 3")
+def p_sentence(p):
+    """sentence : statement
+                | relation
+                | arithmetic
+                | factor
+                | expression  """
 
 
 def p_empty(p):
-    """empty : """
+    '''empty :'''
     pass
 
 
 def p_error(p):
-    print ("error de sintaxis", p)
-    print ("error en la linea " + str(p.lineno))
+    print("Error de sintaxis ", p)
 
 
 parser = yacc.yacc()
-result = parser.parse(cadena)
-print (result)
+#result = parser.parse(cadena)
+#print (result)
